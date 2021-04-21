ac8f85623e243bc723fa5e1f910460ce67f7122f
Danny Ospino <Dannii.ospino@gmail.com>
2021-04-21 17:37:00 -0500

    new files in the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:LayoutComponent,
+    children:[
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./home/home.module').then(
+            (m) => m.HomeModule
+          ),
+      },
+      {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'home'
+      },
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
